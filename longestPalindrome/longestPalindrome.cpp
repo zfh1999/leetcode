cbdc8cdf3198650c6916a0c2c1e0599c52145b23
zfh1999 <102207552+zfh1999@users.noreply.github.com>
2024-01-09 21:56:20 +0800

    最长回文子串

--- longestPalindrome/longestPalindrome.cpp
+++ longestPalindrome/longestPalindrome.cpp
@@ -1,25 +1,62 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <unordered_map>
+#include <map>
+#include <memory>
 
 class Solution
 {
 public:
-    string longestPalindrome(string s)
+    std::string longestPalindrome(const std::string &s)
     {
-        if(s.size() < 2)
+        std::pair<int, int> max_pair;
+        int max_length = 1;
+        if (s.size() < 2)
         {
             return s;
         }
-        if(s.size() == 2 && s[0] == s[1])
+        if (s.size() == 2 && s[0] == s[1])
         {
             return s;
         }
-        else
+
+        for (int i = 0; i < s.size() - 1; i++)
         {
-            return "";
+            for (int j = i + max_length; j < s.size(); j++)
+            {
+                if(j > s.length())
+                {
+                    break;
+                }
+                if (isPalindrome(s, i, j))
+                {          
+                    max_length = j - i + 1;
+                    max_pair = std::make_pair(i, j);
+                }
+            }
         }
-        
-        
+        return s.substr(max_pair.first, max_pair.second - max_pair.first + 1);
+    }
+
+    bool isPalindrome(const std::string &s, int begin, int end)
+    {
+        while (begin < end)
+        {
+            if (s[begin] != s[end])
+            {
+                return false;
+            }
+            begin++;
+            end--;
+        }
+        return true;
     }
 };
+
+int main()
+{
+    Solution s;
+    auto str{"jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx"};
+    auto ss = s.longestPalindrome(str);
+    std::cout << ss << std::endl;
+}
