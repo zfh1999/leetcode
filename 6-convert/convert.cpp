8bde952f53e97b9cd800a0c3aafd955eda103af6
zfh1999 <102207552+zfh1999@users.noreply.github.com>
2024-01-10 16:06:53 +0800

    z字形变换

--- 6-convert/convert.cpp
+++ 6-convert/convert.cpp
@@ -7,23 +7,34 @@ class Solution
 public:
     std::string convert(const std::string &s, int numRows)
     {
-        std::vector<std::string> matrix;
-        int t = 2 * (numRows - 1);
-        int count = numRows / t;
-        int remainder = numRows % t;
-        if (remainder != 0)
-            count += 1;
-        for (int i = 0; i < count; ++i)
+        if(numRows == 1)
         {
-            for (int j = 0; j < numRows; ++j)
+            return s;
+        }
+        std::vector<std::string> matrix(numRows);
+        int t = 2 * (numRows - 1);
+        bool flag;
+        int count = 0;
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(count == numRows - 1)
             {
-                if(i * t + j > s.size()) break;
-                matrix[j].push_back(s[i * t + j]);
+                flag = false;
             }
-            for (int j = 0; j < numRows - 2; ++j)
+            if(count == 0)
             {
-                if(i * t + numRows) break;
-                matrix[numRows - 1 - j - 1].push_back(s[i * t + numRows]);
+                flag = true;
+            }
+
+            matrix[count] += s[i];
+
+            if(flag)
+            {
+                ++count;
+            }
+            else
+            {
+                --count;
             }
         }
         return [](std::vector<std::string> &s2) -> std::string
@@ -41,5 +52,5 @@ public:
 int main()
 {
     Solution s;
-    std::cout << s.convert("PAYPALISHIRING", 3) << std::endl;
+    std::cout << s.convert("PAYPALISHIRING", 4) << std::endl;
 }
